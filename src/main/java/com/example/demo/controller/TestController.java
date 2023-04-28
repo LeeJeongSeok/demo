b724a2bd465f007d45de6ae862535f440ae9d99a
Lee <ljs0429777@gmail.com>
2023-04-28 16:33:10 +0900

    feat : 각 메뉴별 콘텐츠 생성

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,17 +8,57 @@ public class TestController {
 
 	@GetMapping("/")
 	public String test() {
-		return "index";
+		return "layout/default";
 	}
 
-	@GetMapping("/list")
+	@GetMapping("/member")
+	public String member() {
+		return "layout/member";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "member/login";
+	}
+
+	@GetMapping("/order-list")
 	public String test2() {
 		return "orders/orderList";
+	}
+
+	@GetMapping("/coupon-list")
+	public String couponList() {
+		return "coupon/couponList";
+	}
+
+	@GetMapping("/point-list")
+	public String pointList() {
+		return "point/pointList";
+	}
+
+	@GetMapping("/delivery-list")
+	public String deliveryList() {
+		return "delivery/deliveryList";
+	}
+
+	@GetMapping("/product-complete")
+	public String productComplete() {
+		return "product/productReplyComplete";
+	}
+
+	@GetMapping("/product-stay")
+	public String productStay() {
+		return "product/productReplyStay";
 	}
 
 	@GetMapping("/default")
 	public String test3() {
 		return "layout/default";
+	}
+
+	@GetMapping("/product/list")
+	public String test4() {
+		return "productReplyComplete";
 	}
 
 }
